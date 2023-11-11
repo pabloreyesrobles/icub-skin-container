7998059b49e6a3d432cc3e15b23243a1f5856ab7
Pablo Reyes Robles <pabloreyes500@gmail.com>
2023-11-11 11:17:27 -0300

    script with logs

--- script.sh
+++ script.sh
@@ -1,21 +1,21 @@
 echo 'Starting Yarp Server...'
-yarpserver > /dev/null 2>&1 &
+yarpserver > /dev/null &> ../yarpserver.log &
 
 sleep 1
 echo 'Starting gzserver with iCub skin...'
-gzserver code-icub-gazebo-skin/worlds/icub_skin.world -s libgazebo_yarp_clock.so > /dev/null 2>&1 &
+gzserver code-icub-gazebo-skin/worlds/icub_skin.world -s libgazebo_yarp_clock.so > /dev/null &> ../gzserver.log &
 GAZEBO_PID=$!
 sleep 15
 
-yarprobotinterface --context gazeboCartesianControl --config no_legs.xml > /dev/null 2>&1 &
-iKinCartesianSolver --context gazeboCartesianControl --part right_arm > /dev/null 2>&1 &
-iKinCartesianSolver --context gazeboCartesianControl --part left_arm > /dev/null 2>&1 &
+yarprobotinterface --context gazeboCartesianControl --config no_legs.xml > /dev/null &> ../yarprobotinterface.log & 
+iKinCartesianSolver --context gazeboCartesianControl --part right_arm > /dev/null &> ../iKinCartesianSolver_right_arm.log &
+iKinCartesianSolver --context gazeboCartesianControl --part left_arm > /dev/null &> ../iKinCartesianSolver_left_arm.log &
 
 sleep 10
 echo 'Training will start in 3 seconds'
 sleep 3
 cd HebbianMetaLearning
-python train_hebb.py --environment icub_skin-v0 --generations 100 --popsize 25 --threads 1 --sigma 0.15
+python train_hebb.py --environment icub_skin-v0 --generations 100 --popsize 25 --threads 1 --sigma 0.15 &> ../train_hebb.log &
 
 #iCubSkinGui --from left_arm.ini --useCalibration > /dev/null 2>&1 &
 #iCubSkinGui --from left_forearm.ini --useCalibration > /dev/null 2>&1 &
